75965ec81d92d18aee58af4e90da68c0bac47bf2
Jar'Zeno <jarzenojarrett@gmail.com>
2023-01-12 13:55:41 -0800

    Implement react testing using jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App compnent", () => {
+  test("renders Magnificent Monkeys", () => {
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot()
+  });
+
+  test("renders Radical Rhinos after button click", () => {
+    render(<App />);
+    const button = screen.getByRole("button", { name: "Click Me!" });
+
+    userEvent.click(button);
+
+    expect(screen.getByRole("heading").textContent).toMatch(/Radical Rhinos/i);
+  });
 });
